11c2cd34ee6163f943dc141982de4cd839e412bd
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-27 23:30:21 +0300

    markup

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+player.on('timeupdate', handlerUpdate);
+function handlerUpdate(event) {
+  const videoTime = 'videoplayer-current-time';
+  localStorage.setItem(videoTime, event.seconds);
+}
