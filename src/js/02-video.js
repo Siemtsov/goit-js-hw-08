fb2e16e610300eaad4ce9f0c0adf174d719329cb
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-28 15:16:58 +0300

    remade task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import Player from '@vimeo/player';
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 const videoTime = 'videoplayer-current-time';
-player.setCurrentTime(localStorage.getItem(videoTime));
+player.setCurrentTime(JSON.parse(localStorage.getItem(videoTime)));
 player.on('timeupdate', throttle(handlerUpdate, 1000));
 function handlerUpdate(event) {
   localStorage.setItem(videoTime, JSON.stringify(event.seconds));
