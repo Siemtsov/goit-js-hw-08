1acf36a2eedcb73871db98da34214d7b927e7931
Siemtsov <134514769+Siemtsov@users.noreply.github.com>
2023-10-29 10:07:21 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,9 @@ import Player from '@vimeo/player';
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 const videoTime = 'videoplayer-current-time';
-player.setCurrentTime(JSON.parse(localStorage.getItem(videoTime)));
+if (localStorage.getItem(videoTime) != 0) {
+  player.setVideoTime(localStorage.getItem(videoTime));
+}
 player.on('timeupdate', throttle(handlerUpdate, 1000));
 function handlerUpdate(event) {
   localStorage.setItem(videoTime, JSON.stringify(event.seconds));
